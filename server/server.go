db12d65ef7cf38ff2a2ae92b7274e4d4723adfdc
Alicia Thoney <athoney@uwyo.edu>
2023-05-01 21:25:49 -0600

    add rate limiting

--- server/server.go
+++ server/server.go
@@ -1,19 +1,41 @@
 package main
 
 import (
+	"time"
+
+	ratelimit "github.com/JGLTechnologies/gin-rate-limit"
 	"github.com/gin-gonic/gin"
 	"hibp.ssd.com/api"
 )
 
+func keyFunc(c *gin.Context) string {
+	return c.ClientIP()
+}
+
+func errorHandler(c *gin.Context, info ratelimit.Info) {
+	c.String(429, "Too many requests. Try again in "+time.Until(info.ResetTime).String())
+}
+
 func main() {
 	r := gin.Default()
 	r.NoRoute(api.FOF)
+
+	// This makes it so each ip can only make 5 requests per second
+	store := ratelimit.InMemoryStore(&ratelimit.InMemoryOptions{
+		Rate:  time.Minute,
+		Limit: 5,
+	})
+	mw := ratelimit.RateLimiter(store, &ratelimit.Options{
+		ErrorHandler: errorHandler,
+		KeyFunc:      keyFunc,
+	})
 
 	//Static Routes
 	r.GET("/", api.Home)
 	r.GET("/about", api.About)
 	r.GET("/login", api.Login)
 	r.POST("/login", api.LoginUser)
+	r.POST("/login/email", mw, api.LoginUser)
 	r.GET("/post", api.Post)
 	r.GET("/contact", api.Contact)
 
